dad7313bd89d6909b585c701f83b8bba8b73b5bd
mkissarli <kissarlim@gmail.com>
2020-08-22 18:17:07 +0100

    Update jump_game_2.java

--- jump_game_2.java
+++ jump_game_2.java
@@ -14,7 +14,7 @@
  *
  * https://leetcode.com/problems/jump-game-ii/
  */
-
+/** OLD
 class Solution {
     public int jump(int[] nums) {
         if(nums.length == 1){
@@ -42,3 +42,58 @@ class Solution {
         }
     }
 }
+*/
+
+// Still too slow, but faster still...
+class Solution {
+    //public static int[] count_list = new int[1];
+    
+    public static int jump(int[] nums) {
+        if(nums.length == 1){
+            return 0;
+        }
+        int[] count_list = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            count_list[i] = -1;
+        }
+        for(int i = nums.length - 1; i > 0; i--){
+            count_list[i] = recursion_jumping(nums, i, 0, count_list);
+            //System.out.print(i);System.out.print(" : ");System.out.println(count_list[i]);
+        }
+        return recursion_jumping(nums, 0, 0, count_list);
+    }
+    public static int recursion_jumping(int[] nums, int current_loc, int turns, int[] count_list){
+        if(current_loc >= nums.length - 1){
+            return turns;
+        }
+        
+        else if (nums[current_loc] >= 1){
+            int low = 100000;
+            for(int i = 1; i <= nums[current_loc] && current_loc + i < nums.length; i++){
+                int val = low;
+                if(count_list[current_loc + i] == -1){
+                    val = recursion_jumping(nums, current_loc + i, turns + 1, count_list);
+                    count_list[current_loc + i] = val;
+                }
+                else if(count_list[current_loc + i] == 300000){
+                    val = 300000;
+                }
+                else if(nums[current_loc] + current_loc < nums.length - 1 && nums[current_loc + i] == 300000){
+                    return 300000;
+                }
+                else{
+                    val = recursion_jumping(nums, current_loc + i, turns + 1, count_list);
+                }
+
+                if(val < low){
+                    low = val;
+                }
+            }
+            return low;
+        }
+        else{
+            return 200000;
+        }
+    }
+}
+//[5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5]
